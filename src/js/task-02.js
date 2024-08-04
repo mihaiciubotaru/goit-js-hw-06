371d4362be48c10269261d2715541d8e2f9f1889
mihaiciubotaru <mihai.cpp@gmail.com>
2024-08-04 13:24:10 +0300

    Final task-o2.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listLi = [];
+const ul = document.getElementById("ingredients");
+
+for(let i = 0; i<ingredients.length; i++){
+  let li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent=ingredients[i];
+  listLi.push(li);
+}
+
+for(let currentLi of listLi){ul.appendChild(currentLi)};
+
+
