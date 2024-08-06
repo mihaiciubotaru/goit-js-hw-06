7e9cc8cd555f194510b6a893362574b8089f51b9
mihaiciubotaru <mihai.cpp@gmail.com>
2024-08-06 17:20:22 +0300

    final Ex 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
 
+"use strict";
+
+const formLogin = document.getElementsByClassName("login-form")[0];
+
+formLogin.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formLogin = document.getElementsByClassName("login-form")[0];
+    const elem = formLogin.elements;
+
+    const obj = {
+        email : elem["email"].value,
+        password : elem["password"].value
+    }
+
+    if (obj.email.length == 0 || obj.password.length == 0){
+        alert("Please insert BOTH username and password");
+        return;
+    }
+
+    console.log("email : " + obj.email);
+    console.log("password : " + obj.password);
+
+    formLogin.reset();
+})
