c37043a8cdba1e092fe28860f78d31bb24c844e8
mihaiciubotaru <mihai.cpp@gmail.com>
2024-08-08 13:33:09 +0300

    final ex 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,20 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChange = document.getElementsByClassName("change-color")[0];
+const spanColor = document.getElementsByClassName("color")[0];
+
+let color = getRandomHexColor();
+document.body.style.backgroundColor=color;
+spanColor.textContent=color;
+
+btnChange.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor=color;
+  spanColor.textContent=color;
+})
